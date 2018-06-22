fc0f9ed61fb312a6a2eee40ca014e8df031eed74
Michael Purwin <mpurwin@gmail.com>
2018-06-21 20:40:46 -0400

    Create gladiator db class

--- models.py
+++ models.py
@@ -5,7 +5,18 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-engine = create_engine('sqlite:///database.db')
 
+class Gladiator(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80))
+    img = Column(String)
+    gender = Column(String(12))
+    hairdo = Column(String(20))
+    main_event = Column(String(40))
+    secondary_event = Column(String(40))
+    accessory = Column(String(20))
+
+
+engine = create_engine('sqlite:///database.db')
 
 Base.metadata.create_all(engine)
