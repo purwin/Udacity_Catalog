6ddfb4f360380c87cb0ebda32e94c051d8756d2d
Michael Purwin <mpurwin@gmail.com>
2018-06-21 20:39:46 -0400

    Create item routes

--- views.py
+++ views.py
@@ -13,6 +13,37 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/catalog/<category>')
+def catalog_category(category):
+    return render_template('category.html')
+
+
+@app.route('/catalog/<int:id>')
+def catalog_item(id):
+    return render_template('item.html')
+
+
+@app.route('/catalog/<int:id>/new', methods=['GET', 'POST'])
+def new_item(id):
+    return render_template('item_new.html')
+
+
+@app.route('/catalog/<int:id>/edit', methods=['GET', 'POST'])
+def edit_item(id):
+    return render_template('item_edit.html')
+
+
+@app.route('/catalog/<int:id>/delete', methods=['GET', 'POST'])
+def delete_item(id):
+    return render_template('item_delete.html')
+
 
 if __name__ == '__main__':
     app.debug = True
