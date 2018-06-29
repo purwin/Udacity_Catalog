7103d0afb25d5c3fccb290c3b715e33c550e4960
Michael Purwin <mpurwin@gmail.com>
2018-06-28 21:21:29 -0400

    Convert to book catalog models

--- models.py
+++ models.py
@@ -9,20 +9,60 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
+    library = relationship('Book', secondary='library', backref='user_library', lazy='dynamic')
+    wishlist = relationship('Book', secondary='library', backref='user_wishlist', lazy='dynamic')
 
 
-class Gladiator(Base):
-    __tablename__ = "gladiator"
+class Book(Base):
+    __tablename__ = "book"
     id = Column(Integer, primary_key=True)
-    name = Column(String(80))
-    img = Column(String)
-    gender = Column(String(12))
-    hairdo = Column(String(20))
-    main_event = Column(String(40))
-    secondary_event = Column(String(40))
-    accessory = Column(String(20))
+    title = Column(String(80))
+    cover = Column(String)
+    description = Column(String)
+    authors = relationship('Author', secondary='book_author', backref='books', lazy='dynamic')
+    genres = relationship('Genre', secondary='book_genre', backref='books', lazy='dynamic')
 
 
-engine = create_engine('sqlite:///database.db')
+class Author(Base):
+    __tablename__ = "author"
+    id = Column(Integer, primary_key=True)
+    last_name = Column(String(80))
+    first_name = Column(String(80))
+    # full_name
+    books = relationship('Book', secondary='book_author')
+    bio = Column(String)
+
+
+class Genre(Base):
+    __tablename__ = "genre"
+    id = Column(Integer, primary_key=True)
+    type = Column(String(80))
+    books = relationship('Book', secondary='book_genre')
+
+
+Table('book_author', Base.metadata,
+      Column('book_id', Integer, ForeignKey('book.id')),
+      Column('author_id', Integer, ForeignKey('author.id'))
+     )
+
+
+Table('book_genre', Base.metadata,
+      Column('book_id', Integer, ForeignKey('book.id')),
+      Column('genre_id', Integer, ForeignKey('genre.id'))
+     )
+
+
+Table('library', Base.metadata,
+      Column('user_id', Integer, ForeignKey('user.id')),
+      Column('book_id', Integer, ForeignKey('book.id'))
+     )
+
+Table('wishlist', Base.metadata,
+      Column('user_id', Integer, ForeignKey('user.id')),
+      Column('book_id', Integer, ForeignKey('book.id'))
+     )
+
+
+engine = create_engine('sqlite:///books.db')
 
 Base.metadata.create_all(engine)
