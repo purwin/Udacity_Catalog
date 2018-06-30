5f69f47830851667a223e969dd3335e9c502df51
Michael Purwin <mpurwin@gmail.com>
2018-06-30 09:45:27 -0400

    Add POST functionality to book create def

--- views.py
+++ views.py
@@ -19,58 +19,64 @@ app = Flask(__name__)
 @app.route('/index')
 @app.route('/home')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 
 # route: login
 @app.route('/login')
 def login():
-    pass
+  pass
 
 # route: logout
 def logout():
-    pass
+  pass
 
 # route: category list
 @app.route('/catalog')
 def catalog():
-    pass
+  return "Catalog!"
 
 
 # route: category results
 @app.route('/catalog/category/<category>')
 def catalog_category(category):
-    genre = session.query(Genre).filter_by(type = category).one()
-    return render_template('category.html', genre = genre)
+  genre = session.query(Genre).filter_by(type = category).one()
+  return render_template('category.html', genre = genre)
 
 
 # route: item (book)
 @app.route('/catalog/book/<int:id>')
 def catalog_item(id):
-    book = session.query(Book).filter_by(id = id).one()
-    return render_template('book.html', book = book)
+  book = session.query(Book).filter_by(id = id).one()
+  return render_template('book.html', book = book)
 
 
 # route: create item (book)
 @app.route('/catalog/book/new', methods=['GET', 'POST'])
 def new_item():
+  if request.method = "POST":
+    new_book = Book(title=request.form['title'], cover=request.form['cover'], description=request.form['description'])
+    session.add(new_book)
+    session.commit()
+    return redirect(url_for('catalog'))
+  else:
     return render_template('book_new.html')
 
 
 # route: edit item (book)
 @app.route('/catalog/book/<int:id>/edit', methods=['GET', 'POST'])
 def edit_item(id):
-    book = session.query(Book).filter_by(id = id).one()
-    return render_template('book_edit.html', book = book)
+  book = session.query(Book).filter_by(id = id).one()
+  return render_template('book_edit.html', book = book)
 
 
 # route: delete item (book)
 @app.route('/catalog/book/<int:id>/delete', methods=['GET', 'POST'])
 def delete_item(id):
-    book = session.query(Book).filter_by(id = id).one()
-    return render_template('book_delete.html', book = book)
+  book = session.query(Book).filter_by(id = id).one()
+  return render_template('book_delete.html', book = book)
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=8000, threaded = False)
+  app.debug = True
+  app.run(host='0.0.0.0', port=8000, threaded = False)
