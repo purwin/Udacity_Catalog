8b7536965a3b987da92aee9217ccd8667092e931
Michael Purwin <mpurwin@gmail.com>
2018-07-01 15:07:03 -0400

    Add gdisconnect route

--- views.py
+++ views.py
@@ -131,6 +131,37 @@ def gconnect():
     print "done!"
     return output
 
+
+#route: gdisconnect
+# Adapted from https://github.com/udacity/ud330
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print 'In gdisconnect access token is {}'.format(access_token)
+    print 'User name is: {}'.format(login_session['username'])
+    url = 'https://accounts.google.com/o/oauth2/revoke?token={}'.format(login_session['access_token'])
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is {}'.format(result)
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
 # route: logout
 def logout():
   pass
